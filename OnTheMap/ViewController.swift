079f464ea13b7aba898cf1adccd8e2ed76234bac
Yehezkiel Litbagay <yehezkiel.litbagay@dana.id>
2021-09-10 18:48:08 +0700

    Setup new project

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Yehezkiel Litbagay on 10/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
